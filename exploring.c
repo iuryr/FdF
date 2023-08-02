14ac8883dd8804e032e9bfc1c0b16814deaca820
iury <iuryroberto@gmail.com>
2023-08-02 12:54:54 -0300

    handling events with general mlx_hook function

--- exploring.c
+++ exploring.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <X11/keysym.h> //macros com definição de teclas
+#include <X11/X.h> //macros com definição de events e masks
 
 #define WINDOW_WIDTH 600
 #define	WINDOW_HEIGHT 300
@@ -19,10 +20,18 @@ int	handle_no_event(void *data)
 	return (0);
 }
 
-int handle_input(int keysym, t_data *data)
+int handle_keypress(int keysym, t_data *data)
 {
 	if (keysym == XK_Escape)
 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+
+	printf("Keypress: %d\n", keysym);
+	return (0);
+}
+
+int handle_keyrelease(int keysym, void *data)
+{
+	printf("Keyrelease: %d\n", keysym);
 	return (0);
 }
 
@@ -46,7 +55,9 @@ int main(void)
 
 	//preparar hooks
 	mlx_loop_hook(data->mlx_ptr, &handle_no_event, data);
-	mlx_key_hook(data->win_ptr, &handle_input, data);
+	
+	mlx_hook(data->win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, data);
+	mlx_hook(data->win_ptr, KeyPress, KeyPressMask, &handle_keypress, data);
 
 	mlx_loop(data->mlx_ptr);
 
@@ -54,5 +65,4 @@ int main(void)
 	mlx_destroy_display(data->mlx_ptr);
 	free(data->mlx_ptr);
 	free(data);
-
 }
