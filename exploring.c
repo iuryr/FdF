bb19bc2dca5f4b1dd868d4253dc2450600d57d07
iury <iuryroberto@gmail.com>
2023-08-02 11:46:28 -0300

    handled possible errors and leaks

--- exploring.c
+++ exploring.c
@@ -1,17 +1,29 @@
 #include <mlx.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define WINDOW_WIDTH 600
+#define	WINDOW_HEIGHT 300
+#define MLX_ERROR 1
 
 int main(void)
 {
 	void	*mlx_ptr;
-	void	*wnd_ptr;
-	int		key_event;
-	int	(*func_ptr)(void *, void *);
-
-	func_ptr = &mlx_destroy_window;
+	void	*win_ptr;
 	
 	mlx_ptr = mlx_init();
-	wnd_ptr = mlx_new_window(mlx_ptr, 800, 600, "Minha primeira janela");
-	mlx_loop(mlx_ptr);
-	mlx_key_hook(wnd_ptr, (*func_ptr)(mlx_ptr, wnd_ptr), 0xff1b);
+	if (mlx_ptr == NULL)
+		return (MLX_ERROR);
+	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "Minha primeira janela");
+	if (win_ptr == NULL)
+	{
+		free(mlx_ptr);
+		return (MLX_ERROR);
+	}
+	while (1)
+		;
+	mlx_destroy_window(mlx_ptr, win_ptr);
+	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
+
 }
