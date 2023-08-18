df048cc057f03215b78c42cd20b9ecb1ddcddc4f
iury <iuryroberto@gmail.com>
2023-08-18 16:47:51 -0300

    reading number of columns in map file

--- fdf.c
+++ fdf.c
@@ -1,29 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   fdf.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/18 14:56:44 by iusantos          #+#    #+#             */
-/*   Updated: 2023/08/18 16:36:39 by iusantos         ###   ########.fr       */
+/*   Created: 2023/08/18 14:50:46 by iusantos          #+#    #+#             */
+/*   Updated: 2023/08/18 16:40:54 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
+#include "fdf.h"
 
-#include <fcntl.h>
-# include <mlx.h>
-# include "./libft/libft.h"
-
-typedef struct s_point
+int	get_map_ncols(int map_fd)
 {
-	int	x;
-	int	y;
-	int	z;
-	int	color;
-}	t_point;
+	char *first_row;
+	int	ncols;
 
+	first_row = get_next_line(map_fd);
+	ncols = count_word(first_row, ' ');
+	return (ncols);
+}
 
-#endif //_FDF_H_
+int main(int argc, char *argv[])
+{
+	int map_fd;
+	int	ncols;
+	map_fd = open("maps/test_maps/42.fdf", O_RDONLY);
+	ncols = get_map_ncols(map_fd);
+	ft_printf("%i\n", ncols);
+	close(map_fd);
+	
+
+}
