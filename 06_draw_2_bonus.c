e94f48e89b9e66398225055605bf6b9c3b9e9f94
iury <iuryroberto@gmail.com>
2023-10-03 17:10:39 -0300

    off by one re-render error

--- 06_draw_2_bonus.c
+++ 06_draw_2_bonus.c
@@ -16,7 +16,7 @@ void	img_pix_put(t_img *img, int x, int y, int color)
 {
 	char	*pixel;
 
-	if ((x < 0 || x > IMG_WIDTH) || (y < 0 || y > IMG_HEIGHT))
+	if ((x < 0 || x >= IMG_WIDTH) || (y < 0 || y > IMG_HEIGHT))
 		return ;
 	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
 	*(int *)pixel = color;
